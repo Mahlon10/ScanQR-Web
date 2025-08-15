33137c9bd4e7d5a327762d0d98ae480f211e53ae
Mahlon10 <clotteymahlon4@gmail.com>
2025-08-15 17:26:08 +0100

    update3

--- src/app/Pages/Artists/page.jsx
+++ src/app/Pages/Artists/page.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import DigitalProduct from "@/components/Navs/digital-product";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Shop Details Page | NextCommerce Nextjs E-commerce template",
   description: "This is Shop Details Page for NextCommerce Template",
-  // other metadata
 };
+
 
 const DigitalProductPage = () => {
   return (
