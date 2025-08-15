5e3759838e0afdf125287abb90b7038ede37ccca
Mahlon10 <clotteymahlon4@gmail.com>
2025-08-15 17:10:02 +0100

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,18 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 
-export default function RootLayout({ children }:{children: React.ReactNode }){
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
+      <head>
         <link rel="icon" href="https://i.ibb.co/Rp630MQT/scanqr-fav.jpg" sizes="any" />
         <title>ScanQR</title>
         <meta name="description" content="A modern webapp containing profile social links and contacts" />
       </head>
       <body>
-        <Navbar />
         <main>{children}</main>
       </body>
     </html>
   );
 }
+
