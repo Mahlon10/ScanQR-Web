dad97a8a774f9a6cca11c5726f9bee7d552d56c2
Mahlon10 <clotteymahlon4@gmail.com>
2025-08-15 18:17:04 +0100

    update7

--- src/app/Pages/Artists/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Metadata } from "next";
-
-export const metadata = {
-  title: "Shop Details Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Shop Details Page for NextCommerce Template",
-};
-
-
